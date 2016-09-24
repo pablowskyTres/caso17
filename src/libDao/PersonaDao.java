76cacc355be9ee4578cf83acff75a833cebe251c
pablowskyTres <p.badef@gmail.com>
2016-09-23 21:10:37 -0300

    Update listo

--- src/libDao/PersonaDao.java
+++ src/libDao/PersonaDao.java
@@ -52,7 +52,7 @@ public class PersonaDao {
             return false;
         }
         String sql = String.format("INSERT INTO persona (id, rut, dv, nombre1, nombre2, apellido_paterno, apellido_materno, telefono, celular, correo, estado)"
-                + "VALUES (persona_seq.nextval, %d, '%s', '%s', '%s', '%s', '%s', %d, %d, '%s', 1)", p.getRut(), p.getDv(), p.getNombre1(), p.getNombre2(), p.getApellidoPaterno(), p.getApellidoMaterno(), p.getTelefono(), p.getCelular(), p.getCorreo(), 1);
+                + "VALUES (persona_seq.nextval, %d, '%s', '%s', '%s', '%s', '%s', %d, %d, '%s', 1)", p.getRut(), p.getDv(), p.getNombre1(), p.getNombre2(), p.getApellidoPaterno(), p.getApellidoMaterno(), p.getTelefono(), p.getCelular(), p.getCorreo());
         return Sesion.getInstance().sqlEjecutar(sql);
     }
     
@@ -71,6 +71,14 @@ public class PersonaDao {
         return false;
     }
     
-    
+    public Boolean updatePersona(Persona p) throws SQLException{
+        String sql = String.format("UPDATE PERSONA SET rut = %d, dv = '%s', nombre1 = '%s', "
+                + "nombre2 = '%s', apellido_paterno = '%s', apellido_materno = '%s', "
+                + "telefono = %d, celular = %d, correo = '%s'",
+                + p.getRut(), p.getDv(), p.getNombre1(), p.getNombre2(), p.getApellidoPaterno(), p.getApellidoMaterno(), 
+                p.getTelefono(), p.getCelular(), p.getCorreo());
+        System.out.println(sql);
+        return Sesion.getInstance().sqlEjecutar(sql);
+    }
     
 }
